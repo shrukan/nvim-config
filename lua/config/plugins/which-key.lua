16b0a66f3710aab49a426569c7bd44b038deb625
Colin Dirren <colin.dirren@gmail.com>
2025-03-30 11:45:51 +0200

    refactor(which-key): change description

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -1,16 +1,19 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
+	opts = {},
 	config = function()
 		local wk = require("which-key")
 		wk.add({
-			{ "<leader>e", group = "Explorer" },
+			{ "<leader>e", group = "Filesystem" },
+
 			{ "<leader>g", group = "Git" },
+			{ "<leader>gt", group = "Toggle" },
+
+			{ "<leader>c", group = "Code" },
+			{ "<leader>ct", group = "Toggle" },
+
+			{ "<leader>f", group = "Find" },
 		})
 	end,
 	keys = {
@@ -19,7 +22,7 @@ return {
 			function()
 				require("which-key").show({ global = false })
 			end,
-			desc = "Buffer Local Keymaps (which-key)",
+			desc = "Show local keymaps",
 		},
 	},
 }
