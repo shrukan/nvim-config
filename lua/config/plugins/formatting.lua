f45334082f6a163db5e3ffc40b82b3a14d20e13c
Colin Dirren <colin.dirren@gmail.com>
2025-03-30 11:48:54 +0200

    feat(formatting): add prettier

--- lua/config/plugins/formatting.lua
+++ lua/config/plugins/formatting.lua
@@ -1,29 +1,37 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local conform = require("conform")
 
 		conform.setup({
-			formatter_by_ft = {
+			formatters_by_ft = {
+				go = { "gofumpt", "gci", "golines" },
 				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
-				go = { "gofumpt" },
+				markdown = { "prettier" },
 			},
-			format_on_save = {
+			format_after_save = {
 				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			}
+				async = true,
+				timeout_ms = 2000,
+			},
+			-- conform will notify you when no formatters are available for the buffer
+			notify_no_formatters = true,
 		})
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		vim.keymap.set({ "n", "v" }, "<leader>cf", function()
 			conform.format({
 				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
+				async = true,
+				timeout_ms = 2000,
 			})
 		end, { desc = "Format file or range (in visual mode)" })
-	end
+	end,
 }
