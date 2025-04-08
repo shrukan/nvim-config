ba741fd466b246524da733b9de54d26a2a5c102b
Colin Dirren <colin.dirren@gmail.com>
2025-04-08 10:40:20 +0200

    style(telescope): formatter defined style

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,42 +1,47 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
---     { "nvim-telescope/telescope-fzf-native.nvim",
---         build = "make",
---         config = function()
---           require("telescope").load_extension("fzf")
---         end,build = "make" },
-  },
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		--     { "nvim-telescope/telescope-fzf-native.nvim",
+		--         build = "make",
+		--         config = function()
+		--           require("telescope").load_extension("fzf")
+		--         end,build = "make" },
+	},
 
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local transform_mod = require("telescope.actions.mt").transform_mod
-    
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local transform_mod = require("telescope.actions.mt").transform_mod
 
-    -- telescope.load_extension("fzf")
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- telescope.load_extension("fzf")
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-  end,
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Files in cwd" })
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "String in cwd" })
+		keymap.set(
+			"n",
+			"<leader>fc",
+			"<cmd>Telescope grep_string initial_mode=normal<cr>",
+			{ desc = "String under cursor in cwd" }
+		)
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope initial_mode=normal<cr>", { desc = "Todos" })
+	end,
 }
