e6d0841a315a326850ab2408222a12723e1c1434
Colin Dirren <colin.dirren@gmail.com>
2025-04-15 11:42:48 +0200

    feat(lsp-lines): add package for detailed diagnostics

---
+++ lua/config/plugins/lsp/lsp-lines.lua
@@ -0,0 +1,24 @@
+return {
+	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+	config = function()
+		local lsp_lines = require("lsp_lines")
+
+		lsp_lines.toggle()
+
+		vim.diagnostic.config({
+			virtual_text = true,
+		})
+
+		Virtual_text = true
+
+		lsp_lines.setup(vim.keymap.set("", "<Leader>cll", function()
+			lsp_lines.toggle()
+
+			Virtual_text = not Virtual_text
+
+			vim.diagnostic.config({
+				virtual_text = Virtual_text,
+			})
+		end, { desc = "Toggle detailed line diagnostics" }))
+	end,
+}
