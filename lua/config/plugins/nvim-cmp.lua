1aa68d892731ace547deaabf0cf00774aa62ef6d
Colin Dirren <colin.dirren@gmail.com>
2025-04-15 11:43:18 +0200

    feat(nvim-cmp): add signature help source

--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -1,12 +1,13 @@
 return {
 	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
+	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-nvim-lsp-signature-help", -- source for function signature
 		"L3MON4D3/LuaSnip", -- snippet engine
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets 
+		"rafamadriz/friendly-snippets", -- useful snippets
 	},
 
 	config = function()
@@ -20,40 +21,43 @@ return {
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
+
 			-- configure how nvim-cmp interacts with snippet engine
 			snippet = {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
 			},
+
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestion 
-				["<C-e>"] = cmp.mapping.abort(), -- close completion suggestion 
-				["<CR>"] = cmp.mapping.confirm({select = false}),
+				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestion
+				["<C-e>"] = cmp.mapping.abort(), -- close completion suggestion
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
 				-- confirm with tab, and if no entry is selected, will confirm the first item
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						local entry = cmp.get_selected_entry()
-					if not entry then
-					  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-					end
+						if not entry then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						end
 						cmp.confirm()
 					else
 						fallback()
 					end
-				end, {"i","s","c",}),
-
+				end, { "i", "s", "c" }),
 			}),
+
 			-- sources for autocompletion
 			sources = cmp.config.sources({
-				{name = "nvim_lsp"}, -- snippets
-				{name = "luasnip"}, -- snippets
-				{name = "buffer"}, -- text within current buffer
-				{name = "path"}, -- file system paths 
+				{ name = "nvim_lsp" }, -- snippets
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+				{ name = "nvim_lsp_signature_help" },
 			}),
 		})
 	end,
