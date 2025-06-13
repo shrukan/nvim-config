34b3b5db8684d561606a0bfae40481acd1236bb2
Colin Dirren <colin.dirren@gmail.com>
2025-06-13 20:30:19 +0200

    feat(catppuccin): add plugin integrations

--- lua/config/plugins/catppuccin.lua
+++ lua/config/plugins/catppuccin.lua
@@ -1,10 +1,18 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha",
+			integration = {
+				cmp = true,
+				gitsigns = true,
+				telescope = true,
+				treesitter = true,
+				which_key = true,
+				mason = true,
+			},
 		})
-  end,
+	end,
 }
