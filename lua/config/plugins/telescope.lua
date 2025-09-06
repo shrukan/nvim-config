5be9a706121cab5e6a387b110e0a671dc86ff7eb
Colin Dirren <colin.dirren@gmail.com>
2025-09-06 14:31:58 +0200

    feat(telescope): add fzf support

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		--     { "nvim-telescope/telescope-fzf-native.nvim",
 		--         build = "make",
 		--         config = function()
@@ -25,9 +26,18 @@ return {
 					},
 				},
 			},
+			extensions = {
+				fzf = {
+					fuzzy = true, -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+				},
+			},
 		})
 
-		-- telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
