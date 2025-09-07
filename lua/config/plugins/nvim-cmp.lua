47f947854e31a790800373b84c0296c6cd73730e
Colin Dirren <colin.dirren@gmail.com>
2025-09-07 18:15:53 +0200

    feat(nvim-cmp): add support for git and cmdline completion

--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -4,10 +4,15 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-cmdline", -- source for cmdline
 		"hrsh7th/cmp-nvim-lsp-signature-help", -- source for function signature
 		"L3MON4D3/LuaSnip", -- snippet engine
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
+		{ "petertriho/cmp-git", opts = {} }, -- for git
+		opts = function(_, opts)
+			table.insert(opts.sources, { name = "git" })
+		end,
 	},
 
 	config = function()
@@ -16,6 +21,24 @@ return {
 
 		-- loads vscode style snippets from installed plugins
 		require("luasnip.loaders.from_vscode").lazy_load()
+
+		-- configure cmdline autocompletion
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
+
+		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
 
 		cmp.setup({
 			completion = {
@@ -30,13 +53,19 @@ return {
 			},
 
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestion
-				["<C-e>"] = cmp.mapping.abort(), -- close completion suggestion
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }), -- previous suggestion
+				["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }), -- next suggestion
+				["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+				["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }), -- show completion suggestion
+				["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }), -- close completion suggestion
+				["<CR>"] = cmp.mapping(
+					cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+					{ "i" }
+				),
 				-- confirm with tab, and if no entry is selected, will confirm the first item
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
