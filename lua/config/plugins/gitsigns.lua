8d83a8918daa0843bb649c1e017db9a886c0937b
Colin Dirren <colin.dirren@gmail.com>
2025-09-07 18:12:19 +0200

    refactor(gitsigns): change description of keymaps

--- lua/config/plugins/gitsigns.lua
+++ lua/config/plugins/gitsigns.lua
@@ -3,65 +3,64 @@ return {
 	event = "BufReadPre",
 
 	config = function()
-		require('gitsigns').setup {
-			signs                        = {
-				add          = { text = '┃' },
-				change       = { text = '┃' },
-				delete       = { text = '_' },
-				topdelete    = { text = '‾' },
-				changedelete = { text = '~' },
-				untracked    = { text = '┆' },
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
 			},
-			signs_staged                 = {
-				add          = { text = '┃' },
-				change       = { text = '┃' },
-				delete       = { text = '_' },
-				topdelete    = { text = '‾' },
-				changedelete = { text = '~' },
-				untracked    = { text = '┆' },
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
 			},
-			signs_staged_enable          = true,
-			signcolumn                   = false, -- Toggle with `:Gitsigns toggle_signs`
-			numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
-			linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
-			word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
-			watch_gitdir                 = {
-				follow_files = true
+			signs_staged_enable = true,
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				follow_files = true,
 			},
-			auto_attach                  = true,
-			attach_to_untracked          = false,
-			current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-			current_line_blame_opts      = {
+			auto_attach = true,
+			attach_to_untracked = false,
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
 				virt_text = true,
-				virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-				delay = 1000,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 100,
 				ignore_whitespace = false,
 				virt_text_priority = 100,
 				use_focus = false,
 			},
 
-			current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-			sign_priority                = 6,
-			update_debounce              = 100,
-			status_formatter             = nil, -- Use default
-			max_file_length              = 40000, -- Disable if file is longer than this (in lines)
-			preview_config               = {
+			current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
 				-- Options passed to nvim_open_win
-				border = 'single',
-				style = 'minimal',
-				relative = 'cursor',
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
 				row = 0,
-				col = 1
+				col = 1,
 			},
-		}
+		})
 
 		-- keymaps
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>gb", "<cmd>Gitsigns blame<CR>", { desc = "Blame" })
 		keymap.set("n", "<leader>gts", "<cmd>Gitsigns toggle_signs<CR>", { desc = "Signs" })
-		keymap.set("n", "<leader>gtw", "<cmd>Gitsigns toggle_word_diff<CR>", { desc = "Words" })
-		keymap.set("n", "<leader>gtb", "<cmd>Gitsigns toggle_current_line_blame<CR>",
-			{ desc = "Line blame" })
+		keymap.set("n", "<leader>gtw", "<cmd>Gitsigns toggle_word_diff<CR>", { desc = "Word differenes" })
+		keymap.set("n", "<leader>gtb", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Line blame" })
 		keymap.set("n", "<leader>gtd", "<cmd>Gitsigns toggle_deleted<CR>", { desc = "Deleted" })
 	end,
 }
