005eb0aa9c835d68313c4c8ee2751a16c5b48145
Colin Dirren <colin.dirren@gmail.com>
2025-09-07 18:06:15 +0200

    feat(linting): add hadolint (dockerfile linter)

--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -16,6 +16,7 @@ return {
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			htmlangular = { "eslint_d" },
+			dockerfile = { "hadolint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
--- lua/config/plugins/lsp/mason.lua
+++ lua/config/plugins/lsp/mason.lua
@@ -4,11 +4,9 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
-		-- import mason
 		local mason = require("mason")
-
 		local mason_tool_installer = require("mason-tool-installer")
-		-- enable mason and configure icons
+
 		mason.setup({
 			ui = {
 				icons = {
@@ -50,6 +48,7 @@ return {
 				{ "golangci-lint", condition = has_go }, -- go
 				"eslint_d", -- web
 				"selene", -- lua
+				"hadolint", -- dockerfile
 			},
 
 			automatic_installation = true,
