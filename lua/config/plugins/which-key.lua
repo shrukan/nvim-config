d6e12505837337b844c3db2cdd113c378ffd4b20
Colin Dirren <colin.dirren@gmail.com>
2025-10-28 20:49:56 +0100

    style(which-key): whitespaces

--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -14,23 +14,26 @@ return {
 			},
 		})
 		wk.add({
-
 			{ "<leader>c", group = "Code" },
 			{ "<leader>cc", group = "Calls" },
 			{ "<leader>cf", group = "Finder" },
 			{ "<leader>ci", group = "Information" },
 
 			{ "<leader>e", group = "Filesystem" },
+
 			{ "<leader>f", group = "Search" },
 
 			{ "<leader>m", group = "Markview" },
 			{ "<leader>g", group = "Git" },
 			{ "<leader>gt", group = "Toggle" },
+
 			{ "<leader>m", group = "Markview", icon = "" },
 			{ "<leader>mc", group = "Checkbox" },
 			{ "<leader>me", group = "Editor" },
 			{ "<leader>ms", group = "Split" },
+
 			{ "<leader>s", group = "Settings", icon = "" },
+
 			{ "<leader>t", group = "Test", icon = "" },
 		})
 	end,
