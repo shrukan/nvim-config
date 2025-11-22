c6b74b763161ed10af98a9e629dbbc0b738259ba
Colin Dirren <colin.dirren@gmail.com>
2025-11-22 14:28:40 +0100

    refactor(alpha): remove plugin

--- lua/config/plugins/alpha-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"goolord/alpha-nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local startify = require("alpha.themes.theta")
-		startify.file_icons.provider = "devicons"
-		require("alpha").setup(startify.config)
-	end,
-}
