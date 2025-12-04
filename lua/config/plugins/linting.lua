39484686423d6100713960af996b028f24ef8db6
Colin Dirren <colin.dirren@gmail.com>
2025-12-04 16:57:54 +0100

    fix(linting): correct root for linters

--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -7,6 +7,19 @@ return {
 	config = function()
 		local lint = require("lint")
 
+		local function get_lsp_root()
+			-- Get all clients attached to the current buffer (0)
+			local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+			for _, client in ipairs(clients) do
+				if client.root_dir then
+					return client.root_dir
+				end
+			end
+			-- FALLBACK: If no LSP is attached yet, fall back to file directory
+			return vim.loop.cwd()
+		end
+
 		lint.linters_by_ft = {
 			lua = { "selene" },
 			go = { "golangcilint" },
@@ -25,15 +38,15 @@ return {
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		vim.api.nvim_create_autocmd({ "LspAttach", "BufWritePost" }, {
 			group = lint_augroup,
 			callback = function()
-				lint.try_lint()
+				lint.try_lint(nil, { cwd = get_lsp_root() })
 			end,
 		})
 
 		vim.keymap.set("n", "<leader>cl", function()
-			lint.try_lint()
+			lint.try_lint(nil, { cwd = get_lsp_root() })
 		end, { desc = "Trigger linting for current file" })
 	end,
 }
