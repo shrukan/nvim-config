4e2a1c82331804d55a9e4242290f1f7b4449f0ed
Colin Dirren <colin.dirren@gmail.com>
2026-01-18 22:02:53 +0100

    fix(theme): update onedark theme

--- lua/config/plugins/colorschemes/onedark.lua
+++ lua/config/plugins/colorschemes/onedark.lua
@@ -10,12 +10,6 @@ return {
 				fg = "#b7c6e2",
 			},
 			highlights = {
-				-- Tags
-				["@tag"] = { fg = "$red" }, -- Tags like HTML tag names.
-				["@tag.builtin"] = { fg = "$red" }, -- JSX tag names.
-				["@tag.attribute"] = { fg = "$yellow", style = { "italic" } }, -- XML/HTML attributes (foo in foo="bar").
-				["@tag.delimiter"] = { fg = "$dark_red" }, -- Tag delimiter
-
 				["@nospell"] = { fg = "none" },
 				["@spell"] = { fg = "none" },
 			},
