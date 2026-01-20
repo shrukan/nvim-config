ab42fb34b2365589853b3d58b861666ce6b5af8c
Colin Dirren <colin.dirren@gmail.com>
2026-01-20 19:55:16 +0100

    fix(linting): not use linting during LspAttach

--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -38,7 +38,7 @@ return {
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "LspAttach", "BufWritePost" }, {
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			group = lint_augroup,
 			callback = function()
 				lint.try_lint(nil, { cwd = get_lsp_root() })
