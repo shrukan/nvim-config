4927ed7421139d93e95b100bf1d5cf40d0a3fbf4
Colin Dirren <colin.dirren@gmail.com>
2026-03-25 10:48:35 +0100

    style(onedark): remove comment

--- lua/config/plugins/colorschemes/onedark.lua
+++ lua/config/plugins/colorschemes/onedark.lua
@@ -1,6 +1,6 @@
 return {
 	"navarasu/onedark.nvim",
-	priority = 1000, -- make sure to load this before all the other start plugins
+	priority = 1000,
 	lazy = false,
 	config = function()
 		require("onedark").setup({
