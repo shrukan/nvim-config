785e45c18397099bcf70e4dfcc3e24e59a29eaa4
Colin Dirren <colin.dirren@gmail.com>
2026-03-25 10:42:47 +0100

    feat(treesitter): add support for python

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -12,26 +12,23 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	config = function()
-		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter.configs")
 
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
+		treesitter.setup({
 			highlight = {
 				enable = true,
 			},
-			-- enable indentation
 			indent = { enable = true },
 
 			-- ensure that all parser are installed sequentially (used in image)
 			sync_install = true,
-			-- ensure these language parsers are installed
 			ensure_installed = {
 				"go",
 				"gomod",
 				"gosum",
 				"gowork",
 				"lua",
+				"python",
 				"javascript",
 				"typescript",
 				"angular",
